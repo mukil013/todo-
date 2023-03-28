389653685b76beebefbda8d21104e9a704dc66b4
mukil013 <mukilmukil1947@gmail.com>
2023-03-28 17:03:38 +0100

    working!

--- js/app.js
+++ js/app.js
@@ -1,11 +1,29 @@
 let background = document.querySelector("body");
 let container = document.querySelector(".container");
 let card = document.querySelector(".container1");
+let input = document.querySelector("#taskin");
 
-function moving_bg(){
-    background.style.backgroundSize = "150%";
-    background.style.backdropFilter = "blur(1rem)";
-    container.style.top = "-130%";
-    card.style.bottom = "1%";
-    
+document.addEventListener("click", movingBg);
+input.addEventListener("click" , movingBg);
+window.addEventListener("load" , loader());
+
+function movingBg(){
+    if(input === document.activeElement){
+        background.style.backgroundSize = "200%";
+        background.style.backdropFilter = "blur(5rem)";
+        container.style.top = "-130%";
+        card.style.bottom = "1%";
+        console.log("focused");
+    }else{
+        background.style.backgroundSize = "100%";
+        background.style.backdropFilter = "blur(0)";
+        container.style.top = "0%";
+        card.style.bottom = "-100%";
+        console.log("not focused");
+    }
 }
+
+function loader(){
+    document.appendChild(document.querySelector(".loadingScreen"));
+}
+
